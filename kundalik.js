4eb064e2b1124a1060e445141c059644081d0c83
asilbek7201 <asilbek.s.2006@gmail.com>
2021-09-06 18:10:28 +0500

    finish kundalik

---
+++ kundalik.js
@@ -0,0 +1,12 @@
+let savol = prompt('Hafta kuniini tanlang')
+
+
+for(let i=0; i< savol.length; i++){
+    if(savol== "dushanba"){
+        alert("INGLIZ TILI , INFORMARIKA , GEOGRAFIYA , BIOLOGIYA , O'ZBEKISTON TARIXI , KIMYO")
+    } else if(savol == "seshanba"){
+        alert("INFORMATIKA , KIMYO , IQTISOD , ALGEBRA , GEOGRAFIYA , JISMONIY TARBIYA")
+    } else{
+        alert("BOOYAH")
+    }
+}
